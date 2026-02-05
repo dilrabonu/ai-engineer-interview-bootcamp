667599c56f0b75f6f68cd7b65afd584174f904ba
Dilrabo Khidirova <iftihorbekd@gmail.com>
2026-02-05 19:58:43 +0500

    Updated

--- day_04/err_handle.py
+++ day_04/err_handle.py
@@ -1,9 +1,20 @@
-number = int("abs")
 
 try:
-    number = int("abs")
+    file = open("missing.text", "r")
+    number = int("abc")
+except FileNotFoundError:
+    print("OOps file doesn't exist!")
 except ValueError:
-    print("ooops! that is not a number!")
-    number = 0
-print("Program continues!")
+    print("Can't convert to number")
+except Exception as e:
+    print(f"Something went wrong: {e}")
+
+try:
+    result = 10 /  2
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+else:
+    print(f"result: {result}")
+finally:
+    print("Clean up code here")
 
