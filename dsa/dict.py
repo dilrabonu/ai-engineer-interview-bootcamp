365ddfb66489d6e17c1b9fb1280048a1fae6e358
Dilrabo Khidirova <iftihorbekd@gmail.com>
2026-02-17 21:05:19 +0500

    Updated

--- dsa/dict.py
+++ dsa/dict.py
@@ -31,32 +31,14 @@ freq = Counter(arr)
 
 print(freq)
 
-# N1
-def build_freq_map(arr):
+# find uniq char
+def UniqChar(s):
     freq = {}
-    for num in arr:
-        if num in freq:
-            freq[num] += 1
-        else:
-            freq[num] = 1
-    return freq
-
-def build_freq_map(arr):
-    freq = {}
-    for num in arr:
-        freq[num] = freq.get(num, 0) + 1
-    return freq
-
-# N 3
-from collections import defaultdict
-def build_freq_map(arr):
-    freq = defaultdict(int)
-    for num in arr:
-        freq[num] += 1
-    return dict(freq)
-
-# N 4
-from collections import Counter
-arr = [1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1]
-freq = Counter(arr)
-print(freq)
+    for char in s:
+        freq[char] = freq.get(char, 0) + 1
+    
+    for i, char in enumerate(s):
+        if freq[char] == 1:
+            return i
+    return -1
+    
