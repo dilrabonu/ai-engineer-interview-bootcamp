cd4a2e2061000059fc5f7b4cbb32598f00cfd0da
Dilrabo Khidirova <iftihorbekd@gmail.com>
2026-02-17 19:54:34 +0500

    New files added

---
+++ dsa/array.py
@@ -0,0 +1,11 @@
+nums = [1, 2, 3, 4, 5]
+print(nums[0])
+print(nums[-1])
+
+#adding elements
+nums.append(6)
+nums.insert(0,0)
+
+# delete the elements
+nums.pop(3)
+nums.pop()
--- professional/Day_1/type.py
+++ professional/Day_1/type.py
@@ -1,12 +1,24 @@
-score = 78
-if score > 90:
-    print("Grade: A")
-elif score >80:
-    print("Grade: B")
-elif score > 70:
-    print("Grade: C")
-else:
-    print("Grade: E")
+# score = 78
+# if score > 90:
+#     print("Grade: A")
+# elif score >80:
+#     print("Grade: B")
+# elif score > 70:
+#     print("Grade: C")
+# else:
+#     print("Grade: E")
 
-print("Grade: ", score)
+# print("Grade: ", score)
 
+# # while
+# n = 5
+# while n > 0:
+#     n -= 1
+#     print(n)
+
+for x in[1,2,3,4]:
+    if x == 2:
+        continue
+    if x == 4:
+        break
+    print(x)
