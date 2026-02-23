7157b4320e98501d96ded5974aee3dbc1f7662e7
intel <iftihorbekd@gmail.com>
2026-02-23 20:27:48 +0500

    Updated

--- professional/Day_3/dsa.py
+++ professional/Day_3/dsa.py
@@ -1,3 +1,4 @@
+# Pattern 1
 def twosum(nums, target):
     seen = {}
     for i , num in enumerate(nums):
@@ -7,4 +8,12 @@ def twosum(nums, target):
         seen[num] = I
     return []
 
+# Patter 2 Sliding window
+def max_subarray(nums, k):
+    window_sum = sum(num[:k])
+    max_sum = window_sum
+    for i in range(k, len(nums)):
+        window_sum += nums[i] - nums[i - k] # slide window
+        max_sum = max(max_sum, window_sum)
+    return max_sum
 
