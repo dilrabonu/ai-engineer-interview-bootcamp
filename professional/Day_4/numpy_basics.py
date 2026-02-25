fc98d6337c36b07b385969733413cf4a574c48b7
intel <iftihorbekd@gmail.com>
2026-02-25 21:26:52 +0500

    Updated

--- professional/Day_4/numpy_basics.py
+++ professional/Day_4/numpy_basics.py
@@ -54,3 +54,38 @@ pd.concat([df1, df2], axis=1)
 df.pivot_table(values='income', index='education',
     columns='target', aggfunc='mean')
 pd.melt(wide_df, id_vars='model', var_name='metric', value_name='score')
+
+df['age'].fillna(df['age'],mean())
+df['income'].fillna(df['income'].median())
+df['education'].fillna(df['education'].mode()[0])
+
+df['price'].ffill()
+df['price'].bfill()
+df['price'].interpolate()
+
+group_medians = df.groupby('education')['income'].transform('median')
+df['income'].fillna(group_medians)
+
+# KNN imputation
+from sklearn_impute inmport KNNImputer
+imputer = KNNImputer(n_neighbors=5)
+df_imputed = imputer.fit_transform(df)
+
+df['income_missing'] = df['income'].isnull().astype(int)
+
+# One hot Encoding
+pd.get_dummies(df['city'], prefix='city' drop_first=True)
+
+# Ordinal Encoding
+edu_map = {'high_school': 0, 'bachelors': 1, 'masters': 2, 'phd': 3}
+df['edu_encoded'] = df['education'].map(edu_map)
+
+# Target Encoding
+def target_encode(df, col, target, smoothing=10):
+    global_mean = df[target].mean()
+    agg = df.groupby(col)[target].agg(['mean', 'count'])
+    smoothed = (agg['count']*agg['mean'] + smoothing*global_mean) \
+                / (agg['count'] + smoothing)
+    return df[col].map(smoothed)
+
+    
