94faeafa0a9f0dd389f71dd58f274eb30a296c76
intel <iftihorbekd@gmail.com>
2026-02-26 20:31:15 +0500

    Updated

--- professional/Day_4/project_4.py
+++ professional/Day_4/project_4.py
@@ -14,3 +14,39 @@ repeat for epochs:
     db = (2/n) * sum(y_hat - y)
     w = w -lr *db
 """
+import numpy as np 
+
+class LinearRegressionGD:
+    def __init__(self, lr=0.01, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs 
+        self.w = None
+        self.b = None
+        self.loss_history = []
+    
+    def fit(self, X, y):
+        X = np.asarray(X, dtype=float)
+        y = np.asarray(y, dtype=float).reshape(-1)
+
+        n, d = X.shape
+
+        self.w = np.zeros(d)
+        self.b = 0.0
+
+        for _ in range(self.epochs):
+            y_hat = self.predict(X) #forward pass
+            error = y_hat - y # residuals
+
+            loss = np.mean(error ** 2) # MSE
+            self.loss_history.append(loss)
+
+            # Gradient vectorized
+            dw = (2.0 / n) * (X.T @ error)
+            db = (2.0 / n) * np.sum(error)
+
+            # Gradient descent update
+            self.w -= self.lr * dw
+            self.b -= self.lr * db
+        return self
+
+
