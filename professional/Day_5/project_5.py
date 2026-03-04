430b3e551cd15ffac5e0f7f06c8f523cb11f82ff
intel <iftihorbekd@gmail.com>
2026-03-04 13:34:17 +0500

    feat: Updated

--- professional/Day_5/project_5.py
+++ professional/Day_5/project_5.py
@@ -33,7 +33,7 @@ def generateParanthesis(n: int) -> list[str]:
             return
 
         if open_count < n:
-            backtrack(current + ')', open_count + 1, close_count)
+            backtrack(current + '(', open_count + 1, close_count)
         if close_count < open_count:
             backtrack(current + ')', open_count, close_count +1)
     backtrack('', 0, 0)
